45f20737aa857394e7adc711e36379d77cff2d31
AliciaPM08 <apachecomena@gmail.com>
2025-01-17 20:03:56 +0100

    Hola Mundo en SpringBoot

---
+++ src/test/java/com/example/hola_mundo_spring/HolaMundoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hola_mundo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
