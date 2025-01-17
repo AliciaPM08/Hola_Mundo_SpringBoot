24c4f7c8e3b36fc06f17a9677d76685bbf723944
AliciaPM08 <apachecomena@gmail.com>
2025-01-17 20:05:03 +0100

    Hola Mundo en SpringBoot

--- src/main/java/com/example/hola_mundo_spring/HolaMundo.java
+++ src/main/java/com/example/hola_mundo_spring/HolaMundo.java
@@ -1,4 +1,12 @@
 package com.example.hola_mundo_spring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HolaMundo {
+    @GetMapping("/holii")
+    public String hola() {
+        return "<h1>Hola Mundo<h1>";
+    }
 }
